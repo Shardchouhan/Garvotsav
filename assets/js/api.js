fb9136fa2c944158f3ec9314948c9eb4e1dee9ea
“Sharad <sharadchouhan2004gmail.com>
2026-07-01 22:38:28 +0530

    Update registratioI

--- assets/js/api.js
+++ assets/js/api.js
@@ -4,7 +4,7 @@
  */
 
 // TODO: Replace with your deployed Google Apps Script Web App URL
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzncshXVTws9j4s5MaAeFwZGe8lYLOKPPlDqGWgaO0wn-UjuldbfOnCZGTlwjr0QrA/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby3q5qiHuEtNaiANy6KEIuJ5G6arHESn148_JUrGFUjsE029Mz5uaFFVk9hnjaxjcw/exec";
 const USE_MOCK_DATA = false; // Set to false when connecting real backend
 
 const MockData = {
